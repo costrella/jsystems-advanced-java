2a8290d17deef1e6e7abf727235358763b88e2e4
Michal Schielmann <michalschielmann@gmail.com>
2023-06-08 22:34:40 +0200

    generics solution1 - generic message added

---
+++ src/main/java/pl/jsystems/advancedjava/generics/solutions/s1genericmessage/EXERCISE.md
@@ -0,0 +1,10 @@
+## Ćwiczenie 1.
+1. Zapoznaj się z kodem projektu.
+2. Stwórz parametryzowalną klasę `Message<T>` w paczce `.generics` ('obok' klasy `Box`).
+Klasa ta powinna posiadać pole finalne typu `T` przechowujące treść wiadomości,
+powinna również mieć 'getter' do pobierania treści 
+oraz przesłoniętą metodę `toString()` (IntelliJ potrafi ją wygenerować).
+3. W klasie `GenericsExercise1GenericMessage` utwórz obiekt typu `Message<String>`.
+Wyświetl w logach ten obiekt, oraz niezależnie treść wiadomości (pobraną przez 'getter').
+4. *Napisz kod w duchu TDD.
+5. *Zapewnij, by treść wiadomości nie była pusta.
---
+++ src/main/java/pl/jsystems/advancedjava/generics/solutions/s1genericmessage/GenericsExercise1GenericMessage.java
@@ -0,0 +1,22 @@
+package pl.jsystems.advancedjava.generics.solutions.s1genericmessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class GenericsExercise1GenericMessage
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericsExercise1GenericMessage.class);
+
+    public static void main(String[] args)
+    {
+        new GenericsExercise1GenericMessage().run();
+    }
+
+    private void run()
+    {
+        LOGGER.info("Creating new message...");
+        Message<Integer> message = new Message<>(42);
+        LOGGER.info("Message: {}", message);
+        LOGGER.info("Message content: {}", message.getContent());
+    }
+}
