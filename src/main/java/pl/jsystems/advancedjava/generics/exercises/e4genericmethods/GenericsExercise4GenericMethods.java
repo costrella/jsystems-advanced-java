27e91334b7c5640427d2c7a23b086389619b1b91
Michal Schielmann <michalschielmann@gmail.com>
2023-06-09 13:01:43 +0200

    generics exercise4 - instructions

---
+++ src/main/java/pl/jsystems/advancedjava/generics/exercises/e4genericmethods/GenericsExercise4GenericMethods.java
@@ -0,0 +1,36 @@
+package pl.jsystems.advancedjava.generics.exercises.e4genericmethods;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenericsExercise4GenericMethods
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericsExercise4GenericMethods.class);
+
+    public static void main(String[] args)
+    {
+        new GenericsExercise4GenericMethods().run();
+    }
+
+    private void run()
+    {
+        LOGGER.info("Creating new message using creator...");
+        String firstContent = "First content";
+        String secondContent = "Second content";
+        String thirdContent = "Third content";
+
+        Box<String> box = new Box<>();
+        box.putInto(firstContent);
+
+        LOGGER.info("I have a box! {}", box);
+
+        /*
+        BoxContentReplacer contentReplacer = new BoxContentReplacer();
+        contentReplacer.replaceUsing(box, secondContent);
+        LOGGER.info("I have a box with new content! {}", box);
+
+        String oldNewContent = contentReplacer.replaceUsing(box, thirdContent);
+        LOGGER.info("And I now replaced that content {} with an even newer one! {}", oldNewContent, box);
+        */
+    }
+}
---
+++ src/test/java/pl/jsystems/advancedjava/generics/exercises/e4genericmethods/TestThatGenericBoxShould.java
@@ -0,0 +1,52 @@
+package pl.jsystems.advancedjava.generics.exercises.e4genericmethods;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TestThatGenericBoxShould
+{
+    @Test
+    void beCreatedEmpty()
+    {
+        // GIVEN
+
+        // WHEN
+        Box<String> box = new Box<>();
+
+        // THEN
+        assertThat(box.hasContent()).isFalse();
+    }
+
+    @Test
+    void allowToStoreSomethingOfGivenType()
+    {
+        // GIVEN
+        Box<String> box = new Box<>();
+        String inputContent = "Some content";
+
+        // WHEN
+        box.putInto(inputContent);
+
+        // THEN
+        assertThat(box.hasContent()).isTrue();
+        assertThat(box.peekAtContent()).isEqualTo(inputContent);
+    }
+
+    @Test
+    void allowToGetTheContentOfGivenTypeOut()
+    {
+        // GIVEN
+        Box<String> box = new Box<>();
+        String inputContent = "Some content";
+
+        // WHEN
+        box.putInto(inputContent);
+        String content = box.getOutContent();
+
+        // THEN
+        assertThat(content).isEqualTo(inputContent);
+        assertThat(box.hasContent()).isFalse();
+        assertThat(box.peekAtContent()).isNull();
+    }
+}
