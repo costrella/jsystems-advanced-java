ebae591fb507cd5bff9690d4982ebf5abe0e95c0
Michal Schielmann <michalschielmann@gmail.com>
2023-06-09 12:36:21 +0200

    generics solution3 - generic methods

---
+++ src/main/java/pl/jsystems/advancedjava/generics/solutions/s3genericmethods/GenericsExercise3GenericMethods.java
@@ -0,0 +1,27 @@
+package pl.jsystems.advancedjava.generics.solutions.s3genericmethods;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+
+class GenericsExercise3GenericMethods
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericsExercise3GenericMethods.class);
+
+    public static void main(String[] args)
+    {
+        new GenericsExercise3GenericMethods().run();
+    }
+
+    private void run()
+    {
+        LOGGER.info("Creating new message using creator...");
+        MessageCreator messageCreator = new MessageCreator();
+        messageCreator.createMessageUsing(1L, "my super content");
+        messageCreator.createMessageUsing("second!", "my other content");
+        messageCreator.createMessageUsing("content with super id!");
+        messageCreator.createMessageUsing(3d, 42);
+        messageCreator.createMessageUsing(BigDecimal.ZERO);
+    }
+}
---
+++ src/main/java/pl/jsystems/advancedjava/generics/solutions/s3genericmethods/MessageCreator.java
@@ -0,0 +1,24 @@
+package pl.jsystems.advancedjava.generics.solutions.s3genericmethods;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+class MessageCreator
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageCreator.class);
+
+    <ID, CONTENT> Message<ID, CONTENT> createMessageUsing(ID id, CONTENT content)
+    {
+        Message<ID, CONTENT> message = new Message<>(id, content);
+        LOGGER.info("Created message: {}", message);
+        return message;
+    }
+
+    <CONTENT> Message<UUID, CONTENT> createMessageUsing(CONTENT content) {
+        Message<UUID, CONTENT> message = new Message<>(UUID.randomUUID(), content);
+        LOGGER.info("Created message with UUID: {}", message);
+        return message;
+    }
+}
