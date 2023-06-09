1fc0d3b7e3f876e378af9ea696e301199947a4a6
Michal Schielmann <michalschielmann@gmail.com>
2023-06-09 14:46:54 +0200

    generics exercise5 - instructions

---
+++ src/main/java/pl/jsystems/advancedjava/generics/exercises/e5nestedgenerics/GenericsExercise5NestedGenerics.java
@@ -0,0 +1,19 @@
+package pl.jsystems.advancedjava.generics.exercises.e5nestedgenerics;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenericsExercise5NestedGenerics
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericsExercise5NestedGenerics.class);
+
+    public static void main(String[] args)
+    {
+        new GenericsExercise5NestedGenerics().run();
+    }
+
+    private void run()
+    {
+        LOGGER.info("Let's do some nesting...");
+    }
+}
