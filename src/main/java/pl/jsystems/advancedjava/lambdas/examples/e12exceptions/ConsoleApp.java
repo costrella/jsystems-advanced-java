996c1bb13ad7fa994f27c7a2aaecce9825eba879
Michal Schielmann <michalschielmann@gmail.com>
2023-06-18 16:36:53 +0200

    lambdas example12 - exceptions

---
+++ src/main/java/pl/jsystems/advancedjava/lambdas/examples/e12exceptions/ConsoleApp.java
@@ -0,0 +1,60 @@
+package pl.jsystems.advancedjava.lambdas.examples.e12exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+class ConsoleApp
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsoleApp.class);
+
+    private final BiFunction<String, String, Boolean> inputBiFunction;
+    private final LambdasExample12Exceptions.MyOwnSupplier<String> startTextSupplier;
+    private final Supplier<String> endTextSupplier;
+
+    ConsoleApp(BiFunction<String, String, Boolean> inputBiFunction,
+               LambdasExample12Exceptions.MyOwnSupplier<String> startTextSupplier,
+               Supplier<String> endTextSupplier)
+    {
+        this.inputBiFunction = inputBiFunction;
+        this.startTextSupplier = startTextSupplier;
+        this.endTextSupplier = endTextSupplier;
+    }
+
+    void startConsoleApp()
+    {
+        try
+        {
+            LOGGER.info(startTextSupplier.get());
+        } catch (Exception e)
+        {
+            LOGGER.info("how unlucky! " + e.getMessage());
+            return;
+        }
+        Scanner scanner = new Scanner(System.in);
+
+        String previousInput;
+        String input = null;
+        boolean shouldContinue = true;
+        while (shouldContinue)
+        {
+            previousInput = input;
+            input = scanner.nextLine();
+            shouldContinue = inputBiFunction.apply(input, previousInput);
+        }
+        try
+        {
+            LOGGER.info(endTextSupplier.get());
+        } catch (IllegalStateException e) {
+            LOGGER.error("You made your computer angry! It says: {}", e.getMessage());
+            LOGGER.warn("You better apologise!");
+            while(!"I'm so sorry!!!".equals(input)) {
+                input = scanner.nextLine();
+            }
+            LOGGER.info("There you go - that's much better!");
+        }
+    }
+}
---
+++ src/main/java/pl/jsystems/advancedjava/lambdas/examples/e12exceptions/LambdasExample12Exceptions.java
@@ -0,0 +1,64 @@
+package pl.jsystems.advancedjava.lambdas.examples.e12exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+class LambdasExample12Exceptions
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(LambdasExample12Exceptions.class);
+
+    public static void main(String[] args)
+    {
+        PromptPrinter printer = new PromptPrinter();
+        Instant startTime = Instant.now();
+
+        MyOwnSupplier<String> startTextSupplier = () -> {
+            if (Instant.now().toEpochMilli() % 2L != 0) {
+                throw new Exception("We can only start at even milliseconds epoch time!");
+            }
+            return "Let's start!";
+        };
+
+        Supplier<String> endTextSupplier = () ->
+        {
+            long timePassed = Instant.now().toEpochMilli() - startTime.toEpochMilli();
+            timePassed = timePassed / 1000;
+            if (timePassed < 10)
+            {
+                // we cannot throw checked exceptions since Supplier interface does not have them in 'get' method definition.
+                // throw new Exception("You should play for at least 10s!");
+                throw new IllegalStateException("You should play for at least 10s!");
+            }
+            return "Ending! We've talked for: " + timePassed + " seconds!";
+        };
+
+        new ConsoleApp(printer::displayPromptFor, startTextSupplier, endTextSupplier).startConsoleApp();
+    }
+
+    interface MyOwnSupplier<T> {
+        T get() throws Exception;
+    }
+
+    private static class PromptPrinter
+    {
+        boolean displayPromptFor(String input, String previousInput)
+        {
+            if (Objects.equals(input, previousInput))
+            {
+                LOGGER.info("You're really boring...");
+                return false;
+            }
+            if (previousInput != null)
+            {
+                LOGGER.info("Last time you've put: {}", previousInput);
+            }
+            LOGGER.info("Now you've said: {}", input);
+            LOGGER.info("What else will you tell me?");
+            return true;
+        }
+    }
+}
